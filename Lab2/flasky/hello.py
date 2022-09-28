b9aeebbced9e8d6cf6eeedba5f03a21f0dd28cd2
Mandy <t23843126@icloud.com>
2022-09-27 21:51:39 -0400

    A3 completed

--- Lab2/flasky/hello.py
+++ Lab2/flasky/hello.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
+from flask_moment import Moment
+
 app = Flask(__name__)
 
 
-
+from datetime import datetime
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
     
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html',name=name)
+    return render_template('user.html',current_time=datetime.utcnow())
     
+moment = Moment(app)
 bootstrap = Bootstrap(app)
+
 
 """
 @app.route('/')
