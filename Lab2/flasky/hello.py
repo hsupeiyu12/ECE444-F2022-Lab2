a29efccc783963a0ee8cbd5861f24029b243231f
Mandy <t23843126@icloud.com>
2022-09-28 00:57:02 -0400

    add email field

--- Lab2/flasky/hello.py
+++ Lab2/flasky/hello.py
@@ -1,3 +1,5 @@
+
+"""
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
@@ -18,6 +20,7 @@ def user(name):
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
+"""
 
 """
 @app.route('/')
@@ -35,3 +38,44 @@ from datetime import datetime
 def index():
     return render_template('index.html',current_time=datetime.utcnow())
 """
+
+
+from flask_moment import Moment
+from datetime import datetime
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+
+#4.1
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+#4.2
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    def validate_email(form, field):
+        if "@" not in field.data:
+            raise ValidationError('Please include an @ in the email address.')
+        if "utoronto" not in field.data:
+            raise ValidationError('Please use a UofT email address.')
+
+#4.4
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+           flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
