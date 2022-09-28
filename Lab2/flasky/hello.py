39dddca2b8a9a36069081f0d25fc98dd1cf4f64a
Mandy <t23843126@icloud.com>
2022-09-27 20:38:59 -0400

    add bootstrap in hello.py

--- Lab2/flasky/hello.py
+++ Lab2/flasky/hello.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
+    
+bootstrap = Bootstrap(app)
+
+"""
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
@@ -8,3 +22,11 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+"""
+
+"""
+from datetime import datetime
+@app.route('/')
+def index():
+    return render_template('index.html',current_time=datetime.utcnow())
+"""
